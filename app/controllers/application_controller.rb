9de0aa53c140199b419dc1dc31f673bcf84abdc7
Your Name <you@example.com>
2020-05-27 23:54:32 +0600

    third commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  config.web_console.whiny_requests = false
 end
